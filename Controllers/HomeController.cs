56bd56f48442ecdf0cd0a1ea69f4626f61e35307
Vu Pham <vuphamhocvienmatma@gmail.com>
2020-10-30 08:48:36 +0700

    Lấy danh sách Khách sạn theo Id Tỉnh thành công

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,5 +19,49 @@ namespace Trippy_Land.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Hàm lấy danh sách KS theo Id của tỉnh
+        /// </summary>
+        /// <param name="Id">Id của Tỉnh</param>
+        /// <returns></returns>
+        public ActionResult GetHotelByProvinceId(int? Id)
+        {
+            IQueryable<KhachSan> lstDanhSachKhachSan = DataProvider.Entities.KhachSans;
+            if (Id.HasValue)
+            {
+                lstDanhSachKhachSan = lstDanhSachKhachSan.Where(b => b.idTinh == Id.Value);
+            }
+            return View(lstDanhSachKhachSan);
+        }
+
+        /// <summary>
+        /// Hàm lấy danh sách thức ăn theo Id của tỉnh
+        /// </summary>
+        /// <param name="Id">Id của Tỉnh</param>
+        /// <returns></returns>
+        public ActionResult GetFoodByProvinceId(int? Id)
+        {
+            IQueryable<MonAn> lstDanhSachMonAn = DataProvider.Entities.MonAns;
+            if (Id.HasValue)
+            {
+                lstDanhSachMonAn = lstDanhSachMonAn.Where(b => b.idTinh == Id.Value);
+            }
+            return View(lstDanhSachMonAn);          
+        }
+
+        /// <summary>
+        /// Hàm lấy danh sách địa điểm theo Id của tỉnh
+        /// </summary>
+        /// <param name="Id">Id của Tỉnh</param>
+        /// <returns></returns>
+        public ActionResult GetLocationByProvinceId(int? Id)
+        {
+            IQueryable<DiaDiem> lstDanhSachDiaDiem = DataProvider.Entities.DiaDiems;
+            if (Id.HasValue)
+            {
+                lstDanhSachDiaDiem = lstDanhSachDiaDiem.Where(b => b.idTinh == Id.Value);
+            }
+            return View(lstDanhSachDiaDiem);
+        }
     }
 }
