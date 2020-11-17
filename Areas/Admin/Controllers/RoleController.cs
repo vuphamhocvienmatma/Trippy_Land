33e39a72fd76af55f6a992074a7ff2f377e823bf
Vu Pham <vuphamhocvienmatma@gmail.com>
2020-11-17 10:53:15 +0700

    ok

--- Areas/Admin/Controllers/RoleController.cs
+++ Areas/Admin/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace Trippy_Land.Areas.Admin.Controllers
 {
     public class RoleController : Controller
     {
+        private static readonly ILog logger =
+           LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         // GET: Admin/Role
         public ActionResult ListRole()
         {
@@ -16,37 +19,46 @@ namespace Trippy_Land.Areas.Admin.Controllers
             return View(lstUserRole);
         }
 
-        public JsonResult AddRole(int Id,string TenRole,string MoTa)
+        public JsonResult AddRole(int Id, string TenRole, string MoTa)
         {
-            if(ModelState.IsValid)
+            try
             {
-                UserRole ObjRole = null;
-                bool isAdd = true;
-                if(Id >0)
+                if (ModelState.IsValid)
                 {
-                    ObjRole = DataProvider.Entities.UserRoles.Find(Id);
+                    UserRole ObjRole = null;
+                    bool isAdd = true;
+                    if (Id > 0)
+                    {
+                        ObjRole = DataProvider.Entities.UserRoles.Find(Id);
+                    }
+                    if (ObjRole != null)
+                    {
+                        isAdd = false;
+                    }
+                    else
+                    {
+                        ObjRole = new UserRole();
+                    }
+                    //lấy thông tin từ form
+                    ObjRole.Id = Id;
+                    ObjRole.MoTa = MoTa;
+                    ObjRole.TenRole = TenRole;
+
+                    if (isAdd)
+                    {
+                        DataProvider.Entities.UserRoles.Add(ObjRole);
+                    }
+                    DataProvider.Entities.SaveChanges();
+                    return Json(ObjRole.TenRole, JsonRequestBehavior.AllowGet);
                 }
-                if (ObjRole != null)
-                {
-                    isAdd = false;
-                }
-                else
-                {
-                    ObjRole = new UserRole();
-                }
-                //lấy thông tin từ form
-                ObjRole.Id = Id;
-                ObjRole.MoTa = MoTa;
-                ObjRole.TenRole = TenRole;
-                if (isAdd)
-                {
-                    DataProvider.Entities.UserRoles.Add(ObjRole);
-                }
-                DataProvider.Entities.SaveChanges();
-                return Json(ObjRole.TenRole, JsonRequestBehavior.AllowGet);
-            }    
-            //mặc định trả về rỗng
-            return Json("",JsonRequestBehavior.AllowGet);
+                //mặc định trả về rỗng
+                return Json("", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+                return Json("", JsonRequestBehavior.AllowGet);
+            }
         }
 
         /// <summary>
@@ -56,13 +68,13 @@ namespace Trippy_Land.Areas.Admin.Controllers
         /// <returns></returns>
         public JsonResult GetRole(string Id)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 #region Lấy dữ liệu về loại người dùng
                 UserRole objReturn = null;
                 int RoleId = -1;
                 int.TryParse(Id, out RoleId);
-                if(RoleId != -1)
+                if (RoleId != -1)
                 {
                     UserRole objTemp = DataProvider.Entities.UserRoles.Where(o => o.Id == RoleId).First();
                     objReturn = new UserRole();
@@ -73,7 +85,7 @@ namespace Trippy_Land.Areas.Admin.Controllers
                     objReturn.MoTa = objTemp.MoTa;
                 }
                 #endregion
-                JsonResult jsonRole= Json(objReturn,
+                JsonResult jsonRole = Json(objReturn,
                 JsonRequestBehavior.AllowGet);
                 return jsonRole;
             }
@@ -83,21 +95,30 @@ namespace Trippy_Land.Areas.Admin.Controllers
 
         public JsonResult Remove(string Id)
         {
-            int RoleId = -1;
-            int.TryParse(Id, out RoleId);
-            UserRole objRole = DataProvider.Entities.UserRoles.Where(o => o.Id == RoleId).First();
-            if (objRole != null && objRole.Users.Count > 0)
+            try
             {
-                return Json("Role có người dùng tham chiếu. Xóa hết người dùng thuộc Role trước", JsonRequestBehavior.AllowGet);
-            }
-            if (objRole != null)
-            {
-                DataProvider.Entities.UserRoles.Remove(objRole);
-                DataProvider.Entities.SaveChanges();
+                int RoleId = -1;
+                int.TryParse(Id, out RoleId);
+                UserRole objRole = DataProvider.Entities.UserRoles.Where(o => o.Id == RoleId).First();
+                //if (objRole != null && objRole.Users.Count > 0)
+                //{
+                //    return Json("Role có người dùng tham chiếu. Xóa hết người dùng thuộc Role trước", JsonRequestBehavior.AllowGet);
+                //}
+                if (objRole != null)
+                {
+                    DataProvider.Entities.UserRoles.Remove(objRole);
+                    DataProvider.Entities.SaveChanges();
+                    return Json("", JsonRequestBehavior.AllowGet);
+                }
+                //mặc định trả về rỗng
                 return Json("", JsonRequestBehavior.AllowGet);
             }
-            //mặc định trả về rỗng
-            return Json("", JsonRequestBehavior.AllowGet);
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+                return Json("", JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 }
---
+++ Areas/Admin/Controllers/UserProController.cs
@@ -0,0 +1,62 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Trippy_Land.Models;
+
+namespace Trippy_Land.Areas.Admin.Controllers
+{
+    public class UserProController : Controller
+    {
+        private static readonly ILog logger =
+             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        /// <summary>        
+        /// Chuyển đổi một chuỗi về SHA256
+        /// </summary>
+        /// <param name="clearText">Bản rõ cần băm</param>
+        /// <returns></returns>
+        public static string GetSHA256(string clearText)
+        {
+            SHA256CryptoServiceProvider sHA256 = new SHA256CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(clearText);
+            byte[] targetData = sHA256.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+        //Hiển thị danh sách UserRole
+        public void HienThiDanhSachUserRole(int? idUserRole = null)
+        {
+            List<UserRole> lstUserRole = DataProvider.Entities.UserRoles.ToList();
+
+            ViewBag.UserRole = new SelectList(lstUserRole, "Id", "TenRole", idUserRole.HasValue ? idUserRole.Value : 0);
+        }
+
+        public ActionResult DanhSachUser(string tuKhoa, int? idUserRole)
+        {
+            try
+            {
+                HienThiDanhSachUserRole();
+                IQueryable<User> lstUser = DataProvider.Entities.Users;
+                
+                logger.Info("Have an access to User Page");
+                return View(lstUser);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+                return RedirectToAction("Return", "ErrorPage");
+            }
+
+        }
+    }
+}
