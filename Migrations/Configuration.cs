712f2e3a41a05c78dd88a9e2481a93203d83f889
Vu Pham <vuphamhocvienmatma@gmail.com>
2020-11-18 08:31:02 +0700

    Done

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -11,10 +11,7 @@
 
         protected override void Seed(Trippy_Land.Models.Trippy_Land_Context context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+           
         }
     }
 }
